3250446bc50740f313351ac8a41a86527e8a297d
Piyush Priyam <pr16866@gmail.com>
2023-01-21 22:12:54 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 function App() {
+  const [arr, setArr] = useState([]);
+  const handleClick = () => {
+    let obj = { name: "" };
+    setArr([...arr, obj]);
+  };
+  const remove = (index) => {
+    let val = arr.splice(index, 1);
+    setArr([...arr]);
+    // setArr();
+    console.log(val);
+  };
+  const handleChange = (e, index) => {
+    console.log(e.target.value);
+    arr[index].name = e.target.value;
+    setArr([...arr]);
+  };
+  console.log(arr);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <label htmlFor="name">
+        Name:- <input type="text" className="" placeholder="Name" />
+      </label>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "10px",
+          alignItems: "center",
+          margin: "10px",
+        }}
+      >
+        {arr.map((item, index) => {
+          return (
+            <>
+              <div style={{ display: "flex", gap: "4px" }} key={index}>
+                <label htmlFor="name">
+                  Name:-{" "}
+                  <input
+                    type="text"
+                    className=""
+                    placeholder="Name"
+                    onChange={(e) => handleChange(e, index)}
+                    value={item.name}
+                  />
+                </label>
+                <button onClick={() => remove(index)}>remove</button>
+              </div>
+            </>
+          );
+        })}
+      </div>
+      <button
+        style={{
+          display: "block",
+          textAlign: "center",
+          margin: "auto",
+          marginTop: "20px",
+          cursor: "pointer",
+        }}
+        onClick={handleClick}
+      >
+        Add another field
+      </button>
     </div>
   );
 }
